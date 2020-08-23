547cc8e3e30c855634ef2cdbbe9a59544e0e5aba
Fahimforhad496 <fahimforhad496@gmail.com>
2020-08-23 09:55:02 +0600

    Department CRUD done

--- API/Controllers/DepartmentController.cs
+++ API/Controllers/DepartmentController.cs
@@ -20,79 +20,38 @@ namespace API.Controllers
         }
 
         [HttpGet]
-        public IActionResult GetAll()
+        public async Task<IActionResult> GetAll()
         {
-            return Ok(DepartmentStatic.GetDepartments());
+            return Ok(await _departmentRepository.GetAllAsync());
 
         }
 
         [HttpGet("{code}")]
-        public IActionResult GetA(string code)
+        public async Task<IActionResult> GetA(string code)
         {
-            return Ok(DepartmentStatic.GetADepartment(code));
+            return Ok(await _departmentRepository.GetAsync(code));
         }
 
         [HttpPost]
         public async Task<IActionResult> Insert(Department department)
         {
-            return Ok(await _departmentRepository.InsertDepartment(department));
+            return Ok(await _departmentRepository.InsertDepartmentAsync(department));
         }
 
         [HttpPut("{code}")]
-        public IActionResult Update(string code, Department department)
+        public async Task<IActionResult> Update(string code, Department department)
         {
-            return Ok(DepartmentStatic.UpdateDepartment(code, department));
+            return Ok(await _departmentRepository.UpdateAsync(code, department));
         }
 
         [HttpDelete("{code}")]
-        public IActionResult Delete(string code)
+        public async Task<IActionResult> Delete(string code)
         {
-            return Ok(DepartmentStatic.DeleteDepartment(code));
+            return Ok(await _departmentRepository.DeleteAsync(code));
         }
 
     }
 
-    public static class DepartmentStatic
-    {
-        private static List<Department> Departments { get; set; } = new List<Department>();
-
-        public static Department InsertDepartment(Department department)
-        {
-            Departments.Add(department);
-            return department;
-        }
-
-        public static List<Department> GetDepartments()
-        {
-            return Departments;
-        }
-
-        public static Department GetADepartment(string code)
-        {
-            return Departments.FirstOrDefault(x => x.Code == code);
-        }
-
-        public static Department UpdateDepartment(string code, Department department)
-        {
-            Department result = new Department();
-            foreach (var aDepartment in Departments)
-            {
-                if (code == aDepartment.Code)
-                {
-                    aDepartment.Name = department.Name;
-                    result = aDepartment;
-                }
-
-            }
-
-            return result;
-        }
-
-        public static Department DeleteDepartment(string code)
-        {
-            var department = Departments.FirstOrDefault(x => x.Code == code);
-            Departments = Departments.Where(x => x.Code != department.Code).ToList();
-            return department;
-        }
-    }
+    
+    
 }
--- DLL/Repositories/IDepartmentRepository.cs
+++ DLL/Repositories/IDepartmentRepository.cs
@@ -4,12 +4,18 @@ using System.Text;
 using System.Threading.Tasks;
 using DLL.DatabaseContext;
 using DLL.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace DLL.Repositories
 {
     public interface IDepartmentRepository
     {
-        Task<Department> InsertDepartment(Department department);
+        Task<Department> InsertDepartmentAsync(Department department);
+        Task<List<Department>> GetAllAsync();
+        Task<Department> UpdateAsync(string code, Department department);
+        Task<Department> DeleteAsync(string code);
+        Task<Department> GetAsync(string code);
+
     }
 
     public class DepartmentRepository : IDepartmentRepository
@@ -20,12 +26,46 @@ namespace DLL.Repositories
             _context = context;
         }
 
-        public async Task<Department> InsertDepartment(Department department)
+        public async Task<Department> InsertDepartmentAsync(Department department)
         {
             await _context.Departments.AddAsync(department);
             await _context.SaveChangesAsync();
             return department;
         }
 
+        public async Task<List<Department>> GetAllAsync()
+        {
+            return await _context.Departments.ToListAsync();
+        }
+
+        public async Task<Department> DeleteAsync(string code)
+        {
+            var department = await _context.Departments.FirstOrDefaultAsync(x => x.Code == code);
+
+            _context.Departments.Remove(department);
+            await _context.SaveChangesAsync();
+            return department;
+
+        }
+
+        public async Task<Department> GetAsync(string code)
+        {
+            var department = await _context.Departments.FirstOrDefaultAsync(x => x.Code == code);
+
+           
+            return department;
+
+        }
+
+        public async Task<Department> UpdateAsync(string code, Department department)
+        {
+            var findDepartment = await _context.Departments.FirstOrDefaultAsync(x => x.Code == code);
+            findDepartment.Name = department.Name;
+            findDepartment.Code = department.Code;
+            _context.Departments.Update(findDepartment);
+            await _context.SaveChangesAsync();
+            return department;
+
+        }
     }
 }
